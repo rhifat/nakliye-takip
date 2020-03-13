aace9202a44d7b83293d86b44dc31a8a4d5bdd82
unknown <rhifat@gmail.com>
2020-03-13 17:13:28 +0300

    port duzeltme

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
