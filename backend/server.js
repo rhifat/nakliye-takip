df8e3a86f2ab0ce8e07a98dd0a43edd1fc385473
unknown <rhifat@gmail.com>
2020-03-13 17:28:47 +0300

    duzenleme

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:true });
